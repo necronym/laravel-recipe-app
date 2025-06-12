2042dcf0f0821671cddcf691ae2617acefb22db9
Pilot20111 <Mikhail.gureev2005@gmail.com>
2025-06-12 12:09:56 +0300

    Update input-label.blade.php

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,3 +1,10 @@
+<!-- resources/views/components/input-label.blade.php -->
+
+<!--
+    Label component for form inputs.
+    Accepts either a 'value' prop or slot content.
+-->
+
 @props(['value'])
 
 <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
